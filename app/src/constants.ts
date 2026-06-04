4ef267ecbe500290620d08b7e11be75513757c5e
Subha Sarkar <sarkarsubha1206@gmail.com>
2026-06-04 19:58:01 +0530

    add multi timeline

---
+++ app/src/constants.ts
@@ -0,0 +1,6 @@
+export const FPS = 30;
+export const COMPOSITION_WIDTH = 1280;
+export const COMPOSITION_HEIGHT = 720;
+export const MIN_CLIP_FRAMES = 1;
+export const TRACK_HEIGHT = 56;
+export const DEFAULT_PX_PER_FRAME = 6;
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error('Root element "#root" not found');
+
+createRoot(rootEl).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
